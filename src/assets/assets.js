3d21bcfd006092d912ccfa7dac5d2aab1391dcee
Harsh Chaudhary <harshnimadia@gmail.com>
2025-09-20 05:13:57 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,17 @@
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import messgae_icon from './message_icon.png'
+import question_icon from './question_icon.png'
+import history_icon from './history_icon.png'
+import settings_icon from './settings_icon.png'
+
+
+export const assets = {
+    menu_icon,
+    plus_icon,
+    messgae_icon,
+    question_icon,
+    history_icon,
+    settings_icon,
+
+}
