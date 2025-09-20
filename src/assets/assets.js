22fcaeb45cde342a40bf867afbf6665b14c00636
Harsh Chaudhary <harshnimadia@gmail.com>
2025-09-20 11:23:14 +0530

    🎀 Final Commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,13 @@ import messgae_icon from './message_icon.png'
 import question_icon from './question_icon.png'
 import history_icon from './history_icon.png'
 import settings_icon from './settings_icon.png'
+import user_icon from './user_icon.jpeg'
+import compass_icon from './compass_icon.png'
+import bulb_icon from './bulb_icon.png'
+import code_icon from './code_icon.png'
+import gallary_icon from './gallary_icon.png'
+import mic_icon from './mic_icon.png'
+import send_icon from './send_icon.png'
 
 
 export const assets = {
@@ -13,5 +20,12 @@ export const assets = {
     question_icon,
     history_icon,
     settings_icon,
+    user_icon,
+    compass_icon,
+    bulb_icon,
+    code_icon,
+    gallary_icon,
+    mic_icon,
+    send_icon,
 
 }
---
+++ src/config/gemini.js
@@ -0,0 +1,1 @@
+const apiKey = "AIzaSyBKsSEag3HnHyfpbo8bTRGKjySRiwqsycw";
